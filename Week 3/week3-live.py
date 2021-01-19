de68948674185d4dddbf48c9604612969f62df89
mnavjeev <mnavjeevan@g.ucla.edu>
2021-01-18 19:53:01 -0800

    getting started on week3 materials

---
+++ Week 3/week3-live.py
@@ -0,0 +1,12 @@
+#%% [Markdown]
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %%
+iris = sns.load_dataset('iris')
+iris = pd.read_csv('iris.csv')
+# %%
+iris.head()
+
+# %%
