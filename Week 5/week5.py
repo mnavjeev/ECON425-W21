1712ff73c988208b413c4f61966cfdd94a112182
mnavjeev <mnavjeevan@g.ucla.edu>
2021-02-02 00:31:11 -0800

    Finished the handout for week 5 (more or less), working on the python exercise

---
+++ Week 5/week5.py
@@ -0,0 +1,31 @@
+# Code for Week 5
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+# Import penguins
+penguins = sns.load_dataset("penguins")
+penguins.head()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
